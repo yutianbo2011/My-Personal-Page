23dbd7a42dee2bf0fa07308ddeef98de7d6941e9
Tianbo Yu <yutianbo2011@163.com>
2020-09-24 11:37:34 -0500

    add some projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,28 +1,42 @@
 export const projects = [
   {
     name: "VintageTech Online Shopping",
-    position:"Full-stack Web Developer",
+    position:"Full-stack Web Developer | React.js",
     thumbnail: {
       src: "https://raw.githubusercontent.com/yutianbo2011/My-Profile-Page/master/src/assets/figure/project1.png",
       alt: "VintageTech Online Shopping"
     },
     description: [
-      "Built a RESTful single-page online shopping application with React, Redux, Hooks, and React Routing",
+      "Built a RESTful single-page online shopping application with React, Hooks, and React Routing",
       "Created and verified user, product and order information with Strapi and Cloudinary",
       "Handled credit cards payments functionality from users with Stripe API",
     ],
     url:'https://tianbo-vintage-tec-screen-protector.netlify.app/',
   },
   {
+    name: "TAMUber System at Texas A&M University",
+    position:"Full-stack Web Developer | Ruby",
+    thumbnail: {
+      src: "https://raw.githubusercontent.com/yutianbo2011/My-Profile-Page/master/src/assets/figure/project3.png",
+      alt: "TAMUber System at Texas A&M University"
+    },
+    description: [
+      "Designed a taxi service system to provide an interface for students to book and monitor vehicles and communicate with autonomous vehicles about the booking information with Agile software development methodology",
+      "Responsible for the creation, generation, and presentation of transportation maps, including real-time extraction of vehicle location information from the database, presentation of vehicle locations and estimation vehicle driving routes and arrival time",
+      
+    ],
+    url:'https://app.lucidchart.com/users/registerOrLogin/free?showLogin=false&pseudoEditorTitle=TAMUber+Student&instructionParameter=i18nKey%23%3D%3Achart-web-controllers-invitations-document%23%40%3APN%23%3D%3Aname%23%40%3APV%23%3D%3ATAMUber+Student&invitationId=e25dc6fb-22bd-4fd1-a378-4505932c01ab',
+  },
+  {
     name: "The Burger Ordering System",
-    position:"Front-end Web Developer",
+    position:"Front-end Web Developer | React.js",
     thumbnail: {
       src: "https://raw.githubusercontent.com/yutianbo2011/My-Profile-Page/master/src/assets/figure/project2.png",
       alt: "The Burger Ordering System"
     },
     description: [
       "Built a RESTful single-page online shopping application with React, Redux, Hooks, and React Routing",
-      "Created and verified user, product and order information with Strapi and Cloudinary",
+      "Inproved user experience with animation",
       "Handled credit cards payments functionality from users with Stripe API",
     ],
     url:'https://sharp-wozniak-c04b04.netlify.app/',
